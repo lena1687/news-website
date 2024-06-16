fb42e71af7b63aeb33271bba916ab3ac3bc0a8db
eosedelnikova <eosedelnikova@luxoft.com>
2024-06-16 17:12:06 +0200

    NW-3: header + fortawesome icons

---
+++ src/components/atoms/BaseIcon.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="['icon-text', className]">
+    <font-awesome-icon :icon="iconObject" class="icon" :style="{ fontSize: size }" />
+  </div>
+</template>
+
+<script lang="ts">
+import {computed, defineComponent, PropType} from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { IconName } from '@fortawesome/fontawesome-svg-core';
+import {IconPrefix} from "@fortawesome/free-solid-svg-icons";
+
+export default defineComponent({
+  name: 'BaseIcon',
+  components: {
+    FontAwesomeIcon
+  },
+  props: {
+    iconName: {
+      type: String as PropType<IconName>,
+      required: true
+    },
+    prefix: {
+      type: String as PropType<IconPrefix>,
+      default: 'fa'
+    },
+    size: {
+      type: String,
+      required: false,
+      default: '1rem'
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  setup(props) {
+    const iconObject = computed(() => [props.prefix, props.iconName]);
+    return {
+      iconObject
+    };
+  }
+});
+</script>
---
+++ src/components/molecules/IconText.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="icon-text">
+    <BaseIcon :iconName="iconName" size="2rem" />
+    <span class="text" :style="{ fontSize: textSize }"><slot></slot></span>
+  </div>
+</template>
+
+<script lang="ts">
+import BaseIcon from "@/components/atoms/BaseIcon.vue";
+import { defineComponent, computed, PropType } from "vue";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
+
+export default defineComponent({
+  name: "IconText",
+  components: {
+    BaseIcon,
+  },
+  props: {
+    iconName: {
+      type: String as PropType<IconName>,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "1rem",
+    },
+  },
+  setup(props) {
+    const iconSize = computed(() => props.size);
+    const textSize = computed(() => `calc(${props.size} * 0.75)`);
+
+    return {
+      iconSize,
+      textSize,
+    };
+  },
+});
+</script>
