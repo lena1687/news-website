bd879fd49f88565d6ec3f76aa74b27eb0800ecbc
eosedelnikova <eosedelnikova@luxoft.com>
2024-06-16 17:36:31 +0200

    NW-4: NavMenu.vue

--- src/components/organisms/MainHeader.vue
+++ src/components/organisms/MainHeader.vue
@@ -1,24 +1,31 @@
 <template>
   <header>
-    <IconText iconName="calendar" size="2rem"><CurrentTime/></IconText>
-    <HeaderHeading />
-    <SearchBar />
+    <div>
+      <IconText iconName="calendar" size="2rem"><CurrentTime /></IconText>
+      <HeaderHeading />
+      <SearchBar />
+    </div>
+    <div>
+      <NavMenu />
+    </div>
   </header>
 </template>
 
 <script lang="ts">
-import SearchBar from '../molecules/SearchBar.vue';
+import SearchBar from "../molecules/SearchBar.vue";
 import CurrentTime from "@/components/atoms/CurrentTime.vue";
 import HeaderHeading from "@/components/molecules/HeaderHeading.vue";
 import IconText from "@/components/molecules/IconText.vue";
+import NavMenu from "@/components/molecules/NavMenu.vue";
 
 export default {
-  name: 'MainHeader',
+  name: "MainHeader",
   components: {
+    NavMenu,
     IconText,
     HeaderHeading,
     CurrentTime,
-    SearchBar
-  }
+    SearchBar,
+  },
 };
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,29 @@
-import HomePage from '../components/pages/HomePage.vue';
-import ArticlePage from '../components/pages/ArticlePage.vue';
-import {createRouter, createWebHistory} from "vue-router";
+import HomePage from "../components/pages/HomePage.vue";
+import ArticlePage from "../components/pages/ArticlePage.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import CategoryPage from "@/components/pages/CategoryPage.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'HomePage',
-        component: HomePage
-    },
-    {
-        path: '/article/:id',
-        name: 'ArticlePage',
-        component: ArticlePage
-    }
+  {
+    path: "/",
+    name: "HomePage",
+    component: HomePage,
+  },
+  {
+    path: "/category/:id",
+    name: "CategoryPage",
+    component: CategoryPage,
+  },
+  {
+    path: "/article/:id",
+    name: "ArticlePage",
+    component: ArticlePage,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
